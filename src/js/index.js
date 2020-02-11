5ad0ebe7701a5bea465b76b59f7119da1afae312
avichauhan6832 <avi.r.chauhan6832@gmail.com>
2020-02-11 14:44:01 +0530

    search view complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,41 @@
-// Global app controller
-import x from './test';
-const num = 23;
-console.log(`Imported ${x} from another module. Variable num is ${num}`);
+import Search from './models/Search';
+import {elements, renderLoader, removeLoader} from './views/base';
+import * as searchView from './views/searchView';
+
+const state = {};
+
+const controlSearch = async () => {
+    const query = searchView.getInput();
+    console.log(query);
+
+    if(query) {
+        state.search = new Search(query);
+
+        searchView.clearResult();
+        searchView.clearInput();
+        renderLoader(elements.searchRes);
+
+        await state.search.getResults();    
+
+        console.log(state.search.result);
+
+        removeLoader();
+        searchView.renderRecipes(state.search.result)
+        
+    }
+
+};
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const  btn = e.target.closest('.btn-inline');
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResult();
+        searchView.renderRecipes(state.search.result, goToPage);
+    }
+})
